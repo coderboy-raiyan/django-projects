e1340341db0708d65b7d676a40e5dce69d66b506
Raiyan <tajkierhaque@gmail.com>
2023-11-28 12:11:56 +0600

    django template filters done

--- simple_page_navigation/simple_page_navigation/views.py
+++ simple_page_navigation/simple_page_navigation/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
-def home(request) : 
-    d = {"name" : "Raiyan", "age" : 18, "list" : [1, 2, 3]}
+
+def home(request):
+    d = {
+        "name": "Raiyan", "age": 17, "list": [1, 2, 3],
+        "courses": [
+            {"id": 1, "name": "python", "fee": 5000},
+            {"id": 2, "name": "c++", "fee": 15000},
+            {"id": 3, "name": "Javascript", "fee": 7000}
+        ]
+    }
     return render(request, "home.html", d)
